10d88608620447ae9476f474344a3c3d4311bcf0
pinpong <o.nitzschke@gmail.com>
2016-12-13 16:29:16 +0100

    added SeedRandomGenerator

--- src/main/java/jota/utils/Constants.java
+++ src/main/java/jota/utils/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
 
     public static final String TRYTE_ALPHABET = "9ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+    public static final int SEED_LENGTH_MAX = 81;
+
     public static int addressLengthWithoutChecksum = 81;
     public static int addressLengthWithChecksum = 90;
 
